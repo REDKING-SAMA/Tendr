fe3167adfcb962030d33748a4bbab6094a2e7f7a
Redking <bakreridwan77@gmail.com>
2025-06-06 21:08:09 +0100

    Add files via upload

---
+++ Tendr.js
@@ -0,0 +1,22 @@
+const words = ['Grow.', 'Deliver.', 'Thrive.'];
+const changingWord = document.getElementById('changing-word');
+let index = 0;
+
+setInterval(() => {
+  // Start slide-up-out animation
+  changingWord.classList.add('slide-up-out');
+
+  setTimeout(() => {
+    index = (index + 1) % words.length;
+    changingWord.textContent = words[index];
+
+    changingWord.classList.remove('slide-up-out');
+    changingWord.classList.add('slide-up-in');
+
+    setTimeout(() => {
+      changingWord.classList.remove('slide-up-in');
+    }, 500);
+
+  }, 500);
+
+}, 2000);
