7e48c90082d58599c96904e233d3f02f0cc8a731
Redking <bakreridwan77@gmail.com>
2025-06-09 20:43:42 +0100

    Add files via upload

--- Tendr.js
+++ Tendr.js
@@ -20,3 +20,8 @@ setInterval(() => {
   }, 500);
 
 }, 2000);
+  window.addEventListener("load", () => {
+    const preloader = document.getElementById("preloader");
+    preloader.style.opacity = "0";
+    setTimeout(() => preloader.style.display = "none", 500);
+  });
